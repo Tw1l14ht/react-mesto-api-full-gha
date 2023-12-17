eddec9aea30e26c2084ad4bdf8d94dd9932ee3b1
Rozovika Ivan <ivanrozovika@yandex.ru>
2023-12-17 14:57:02 +0300

    fix

--- backend/middlewares/corsErrors.js
+++ backend/middlewares/corsErrors.js
@@ -1,6 +1,6 @@
 const allowedCors = [
   'https://twilight1.nomoredomainsmonster.ru',
-  'twilight1.nomoredomainsmonster.ru',
+  'https://twilight1.nomoredomainsmonster.ru/signin',
 ];
 
 module.exports = (req, res, next) => {
@@ -10,7 +10,7 @@ module.exports = (req, res, next) => {
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Credentials', true);
   }
 
